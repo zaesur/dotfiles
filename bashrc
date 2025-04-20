42d0b47f4e4587af62b5bdf2664b138bec43cb23
Florian Van Belleghem <fvanbelleghem@icloud.com>
2025-04-20 13:33:39 +0200

    chore: fix dotfiles

--- bashrc
+++ bashrc
@@ -5,5 +5,5 @@ set -o vi
 bind -s "set completion-ignore-case on"
 
 # Add NVIM to PATH
-export PATH="$PATH:/nvim-linux-x86_64/bin"
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 
---
+++ nvim/lua/plugins/tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  lazy = false,
+  config = function()
+    vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+  end
+}
